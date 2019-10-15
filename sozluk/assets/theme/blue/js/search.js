1c149915584b07d8d9a7aeb24b265daf794f80d9
Yavuz Kömeçoğlu <komecoglu.yavuz@gmail.com>
2019-10-15 13:14:33 +0300

    çoklu dil desteği sağlandı

--- sozluk/assets/theme/blue/js/search.js
+++ sozluk/assets/theme/blue/js/search.js
@@ -1,8 +1,7 @@
-$(document).ready(function () {
+$(document).ready(function() {
     console.log("ready!");
 
-    function search()
-    {
+    function search() {
         $('#result').html('');
 
         var language = $("#ddl-languages").val();
@@ -11,17 +10,22 @@ $(document).ready(function () {
         console.log("language", language);
         console.log("search_text", search_text);
 
-        var dict_file = "dictionary-eng.json";
-        if (language == "tr") {
-            dict_file = "dictionary-tr.json"
+        var dict_file = "dictionary-eng-tr.json";
+        if (language == "eng-tr") {
+            dict_file = "dictionary-eng-tr.json"
+        } else if (language == "tr-eng") {
+            dict_file = "dictionary-tr-eng.json"
+        } else if (language == "fr-eng") {
+            dict_file = "dictionary-fr-eng.json"
+        } else if (language == "eng-fr") {
+            dict_file = "dictionary-eng-fr.json"
         }
 
         var expression = new RegExp(search_text, "i");
 
-        if(search_text.length>1)
-        {
-            $.getJSON("assets/data/" + dict_file, function (data) {
-                $.each(data, function (key, value) {
+        if (search_text.length > 1) {
+            $.getJSON("assets/data/" + dict_file, function(data) {
+                $.each(data, function(key, value) {
                     if (expression.test(value.word)) {
                         $('#result').append('<li class="list-group-item link-class"><b>' + value.word + '</b> : ' + value.translation + '</li>');
                     }
@@ -31,11 +35,17 @@ $(document).ready(function () {
     }
 
     $.ajaxSetup({ cache: false });
-    $('#txt-search').keyup(function () {
+    $('#txt-search').keyup(function() {
         search();
     });
 
-    $("#btn-search").click(function () {
+    $("#btn-search").click(function() {
         search();
+    });
+
+    $("#ddl-languages").change(function() {
+        $('#result').html('');
+        $('#txt-search').html('');
+
     });
 });
